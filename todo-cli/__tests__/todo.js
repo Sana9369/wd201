3529ac81b4eb6a4345b54b4c9fd936966973a354
Sana <145995640+Sana9369@users.noreply.github.com>
2023-12-20 13:58:42 +0000

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,3 +1,4 @@
+
 const todoList = require("../todo");
 const { all, add, markAsComplete, overdue, dueToday, dueLater } = todoList();
 describe("Todo List Test suite", () => {
@@ -10,21 +11,38 @@ describe("Todo List Test suite", () => {
   });
   test("Creating a new todo", () => {
     const todoLength = all.length;
-    add({title: "Test todo", dueDate: new Date().toLocaleDateString("en-CA"), completed: false,});
+    add({
+      title: "Test todo",
+      dueDate: new Date().toLocaleDateString("en-CA"),
+      completed: false,
+    });
     expect(all.length).toBe(todoLength + 1);
   });
-
-  test("Todo Complete", () => {markAsComplete(0); expect(all[0].completed).toBe(true);});
-  test("Todo Overdue", () => {add({title: "Test overdue", dueDate: new Date(
+  test("Todo Complete", () => {
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+  test("Todo Overdue", () => {
+    add({
+      title: "Test overdue",
+      dueDate: new Date(
         new Date().setDate(new Date().getDate() - 1)
       ).toLocaleDateString("en-CA"),
-      completed: false,});
+      completed: false,
+    });
     expect(overdue().length).toBe(1);
   });
-  test("Todos due today", () => {expect(dueToday().length).toBe(2);});
+  test("Todos due today", () => {
+    expect(dueToday().length).toBe(2);
+  });
   test("Todos due later", () => {
-    add({title: "Test due later", dueDate: new Date( new Date().setDate(new Date().getDate() + 1)
-      ).toLocaleDateString("en-CA"), completed: false,});
+    add({
+      title: "Test due later",
+      dueDate: new Date(
+        new Date().setDate(new Date().getDate() + 1)
+      ).toLocaleDateString("en-CA"),
+      completed: false,
+    });
     expect(dueLater().length).toBe(1);
   });
 });
